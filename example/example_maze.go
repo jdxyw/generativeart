f4e4376de07e2cd4af20212f0bee1f4d62da2c58
Yongwei Xing <yongwei.xing@liulishuo.com>
2021-03-01 15:06:07 +0800

    format files

--- example/example_maze.go
+++ example/example_maze.go
@@ -3,7 +3,7 @@ package main
 import "generativeart"
 
 func main() {
-	c := generativeart.NewCanva(800, 800, 2,2)
+	c := generativeart.NewCanva(800, 800, 2, 2)
 	c.FillBackgroud(generativeart.Azure)
 	g := generativeart.NewMaze()
 	g.Generative(c, generativeart.Tomato)
